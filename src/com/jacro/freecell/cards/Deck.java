904024354e05c2c11d84214b75f259739941c8db
Paweł <jarco.pawel@gmail.com>
2019-07-25 11:00:11 +0200

    break

--- src/com/jacro/freecell/cards/Deck.java
+++ src/com/jacro/freecell/cards/Deck.java
@@ -1,6 +1,7 @@
 package com.jacro.freecell.cards;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Deck {
@@ -15,5 +16,23 @@ public class Deck {
         }
     }
 
+    public void shuffle(){
+        for (int i = 0; i < 10; i++) {
+            Collections.shuffle(card_deck);
+        }
+    }
 
+    public Card deal(){
+        if (card_deck.isEmpty()) return null;
+        Card card = card_deck.get(0);
+        card_deck.remove(0);
+        return card;
+
+    }
+
+    public void showCards() {
+        for (Card c: card_deck){
+            System.out.println(c);
+        }
+    }
 }
--- src/com/jacro/freecell/cards/Test.java
+++ src/com/jacro/freecell/cards/Test.java
@@ -2,6 +2,24 @@ package com.jacro.freecell.cards;
 
 public class Test {
     public static void main(String[] args) {
+        Deck deck = new Deck();
+//        deck.showCards();
+//        System.out.println();
+//        System.out.println("**************************");
+//        System.out.println();
+//        deck.shuffle();
+//        deck.showCards();
 
+        System.out.println(deck.deal());
+
+        System.out.println();
+        deck.showCards();
+
+        System.out.println();
+
+        System.out.println(deck.deal());
+
+        System.out.println();
+        deck.showCards();
     }
 }
